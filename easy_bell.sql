3d6ef4641f26c2441b265423370753976c90531c
Rahul11-Sharma <rs278743@gmail.com>
2026-02-11 22:18:51 +0530

    add database file

---
+++ easy_bell.sql
@@ -0,0 +1,398 @@
+-- MySQL dump 10.13  Distrib 9.6.0, for macos15.7 (arm64)
+--
+-- Host: localhost    Database: easybell
+-- ------------------------------------------------------
+-- Server version	9.6.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'e6d911ce-ff40-11f0-8e1d-44ad96e7c62a:1-51';
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacts` (
+  `contact_id` int NOT NULL AUTO_INCREMENT,
+  `contact_name` varchar(100) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `contact_type` varchar(100) DEFAULT NULL,
+  `billing_address` varchar(100) DEFAULT NULL,
+  `billing_address_city` varchar(100) DEFAULT NULL,
+  `billing_address_state` varchar(100) DEFAULT NULL,
+  `billing_address_country` varchar(100) DEFAULT NULL,
+  `billing_address_pincode` varchar(100) DEFAULT NULL,
+  `shipping_address` varchar(100) DEFAULT NULL,
+  `shipping_address_city` varchar(100) DEFAULT NULL,
+  `shipping_address_state` varchar(100) DEFAULT NULL,
+  `shipping_address_country` varchar(100) DEFAULT NULL,
+  `shipping_address_pincode` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+INSERT INTO `contacts` VALUES (2,'Rahul Sharma','rahul@gmail.com','Customer','SG Highway','Ahmedabad','Gujarat','India','380015','SG Highway','Ahmedabad','Gujarat','India','380015');
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoice_items`
+--
+
+DROP TABLE IF EXISTS `invoice_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice_items` (
+  `invoice_item_id` int NOT NULL AUTO_INCREMENT,
+  `invoice_item_name` varchar(100) DEFAULT NULL,
+  `invoice_item_unit` int DEFAULT NULL,
+  `invoice_item_quantity` int DEFAULT NULL,
+  `invoice_item_rate` int DEFAULT NULL,
+  `invoice_item_total` int DEFAULT NULL,
+  `invoice_id` int NOT NULL,
+  PRIMARY KEY (`invoice_item_id`),
+  KEY `fk_invoice_items_invoice` (`invoice_id`),
+  KEY `fk_invoice_items_unit` (`invoice_item_unit`),
+  CONSTRAINT `fk_invoice_items_invoice` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_invoice_items_unit` FOREIGN KEY (`invoice_item_unit`) REFERENCES `units` (`unit_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice_items`
+--
+
+LOCK TABLES `invoice_items` WRITE;
+/*!40000 ALTER TABLE `invoice_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoices` (
+  `invoice_id` int NOT NULL AUTO_INCREMENT,
+  `pi_id` int NOT NULL,
+  `invoice_number` varchar(100) DEFAULT NULL,
+  `invoice_status` varchar(100) DEFAULT NULL,
+  `contact_id` int NOT NULL,
+  `invoice_total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`invoice_id`),
+  KEY `fk_invoice_contact` (`contact_id`),
+  KEY `fk_invoice_pi` (`pi_id`),
+  CONSTRAINT `fk_invoice_contact` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_invoice_pi` FOREIGN KEY (`pi_id`) REFERENCES `pi` (`pi_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `item_id` int NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(100) DEFAULT NULL,
+  `item_cost_price` varchar(100) DEFAULT NULL,
+  `item_selling_price` varchar(100) DEFAULT NULL,
+  `item_sku` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (2,'iPhone 15','65000','72000','APL-IP15-BLK');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pi`
+--
+
+DROP TABLE IF EXISTS `pi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pi` (
+  `pi_id` int NOT NULL AUTO_INCREMENT,
+  `quote_id` int NOT NULL,
+  `pi_number` varchar(100) DEFAULT NULL,
+  `pi_status` varchar(100) DEFAULT NULL,
+  `pi_total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `contact_id` int NOT NULL,
+  PRIMARY KEY (`pi_id`),
+  KEY `fk_quote_pi` (`quote_id`),
+  KEY `fk_pi_contact` (`contact_id`),
+  CONSTRAINT `fk_pi_contact` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_quote_pi` FOREIGN KEY (`quote_id`) REFERENCES `quotes` (`quote_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pi`
+--
+
+LOCK TABLES `pi` WRITE;
+/*!40000 ALTER TABLE `pi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pi_items`
+--
+
+DROP TABLE IF EXISTS `pi_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pi_items` (
+  `pi_item_id` int NOT NULL AUTO_INCREMENT,
+  `pi_item_name` varchar(100) DEFAULT NULL,
+  `pi_item_unit` int DEFAULT NULL,
+  `pi_item_quantity` int DEFAULT NULL,
+  `pi_item_rate` int DEFAULT NULL,
+  `pi_item_total` int DEFAULT NULL,
+  `pi_id` int NOT NULL,
+  PRIMARY KEY (`pi_item_id`),
+  KEY `fk_pi_items_pi` (`pi_id`),
+  KEY `fk_pi_items_unit` (`pi_item_unit`),
+  CONSTRAINT `fk_pi_items_pi` FOREIGN KEY (`pi_id`) REFERENCES `PI` (`pi_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_pi_items_unit` FOREIGN KEY (`pi_item_unit`) REFERENCES `units` (`unit_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pi_items`
+--
+
+LOCK TABLES `pi_items` WRITE;
+/*!40000 ALTER TABLE `pi_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pi_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase_order_items`
+--
+
+DROP TABLE IF EXISTS `purchase_order_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase_order_items` (
+  `purchase_order_item_id` int NOT NULL AUTO_INCREMENT,
+  `purchase_order_item_name` varchar(100) DEFAULT NULL,
+  `purchase_order_item_unit` int DEFAULT NULL,
+  `purchase_order_item_quantity` int DEFAULT NULL,
+  `purchase_order_item_rate` int DEFAULT NULL,
+  `purchase_order_item_total` int DEFAULT NULL,
+  `purchase_order_id` int NOT NULL,
+  PRIMARY KEY (`purchase_order_item_id`),
+  KEY `fk_po_items_po` (`purchase_order_id`),
+  KEY `fk_po_items_unit` (`purchase_order_item_quantity`),
+  CONSTRAINT `fk_po_items_po` FOREIGN KEY (`purchase_order_id`) REFERENCES `purchase_orders` (`purchase_order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_po_items_unit` FOREIGN KEY (`purchase_order_item_quantity`) REFERENCES `units` (`unit_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_order_items`
+--
+
+LOCK TABLES `purchase_order_items` WRITE;
+/*!40000 ALTER TABLE `purchase_order_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_order_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase_orders`
+--
+
+DROP TABLE IF EXISTS `purchase_orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase_orders` (
+  `purchase_order_id` int NOT NULL AUTO_INCREMENT,
+  `pi_id` int NOT NULL,
+  `purchase_order_number` varchar(100) DEFAULT NULL,
+  `purchase_order_status` varchar(100) DEFAULT NULL,
+  `vendor_id` int NOT NULL,
+  `purchase_order_total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`purchase_order_id`),
+  KEY `fk_po_contact` (`vendor_id`),
+  KEY `fk_po_pi` (`pi_id`),
+  CONSTRAINT `fk_po_contact` FOREIGN KEY (`vendor_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_po_pi` FOREIGN KEY (`pi_id`) REFERENCES `pi` (`pi_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_orders`
+--
+
+LOCK TABLES `purchase_orders` WRITE;
+/*!40000 ALTER TABLE `purchase_orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quote_items`
+--
+
+DROP TABLE IF EXISTS `quote_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quote_items` (
+  `quote_item_id` int NOT NULL AUTO_INCREMENT,
+  `quote_item_name` varchar(100) DEFAULT NULL,
+  `qutoe_item_unit` int DEFAULT NULL,
+  `quote_item_quantity` int DEFAULT NULL,
+  `quote_item_rate` int DEFAULT NULL,
+  `quote_item_total` int DEFAULT NULL,
+  `quote_id` int DEFAULT NULL,
+  PRIMARY KEY (`quote_item_id`),
+  KEY `fk_quote_items_quote` (`quote_id`),
+  CONSTRAINT `fk_quote_items_quote` FOREIGN KEY (`quote_id`) REFERENCES `quotes` (`quote_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quote_items`
+--
+
+LOCK TABLES `quote_items` WRITE;
+/*!40000 ALTER TABLE `quote_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quote_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quotes`
+--
+
+DROP TABLE IF EXISTS `quotes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quotes` (
+  `quote_id` int NOT NULL AUTO_INCREMENT,
+  `quotes_number` varchar(100) DEFAULT NULL,
+  `quotes_status` varchar(100) DEFAULT NULL,
+  `contact_id` int DEFAULT NULL,
+  `quote_total` decimal(10,2) DEFAULT '0.00',
+  PRIMARY KEY (`quote_id`),
+  KEY `fk_quotes_contact` (`contact_id`),
+  CONSTRAINT `fk_quotes_contact` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quotes`
+--
+
+LOCK TABLES `quotes` WRITE;
+/*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
+INSERT INTO `quotes` VALUES (2,'QT-001','Draft',2,15000.50),(3,'QT-001','Draft',2,15000.50),(4,'QT-001','Draft',2,15000.50);
+/*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `units`
+--
+
+DROP TABLE IF EXISTS `units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `units` (
+  `unit_id` int NOT NULL AUTO_INCREMENT,
+  `unit_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`unit_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `units`
+--
+
+LOCK TABLES `units` WRITE;
+/*!40000 ALTER TABLE `units` DISABLE KEYS */;
+INSERT INTO `units` VALUES (2,'Kg');
+/*!40000 ALTER TABLE `units` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `user_type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Rahul Sharma','rahul@gmail.com','123456','Admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-11 22:09:19
